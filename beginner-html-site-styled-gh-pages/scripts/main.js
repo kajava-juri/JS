8429fd8506d674af71c3f6e465305b41e60f0a75
Jüri Kajava <jyri.kajava@tptlive.ee>
2021-06-04 17:38:30 +0300

    Update main.js

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,13 +1,2 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello!';
-
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute('src','images/conflict.png');
-    } else {
-      myImage.setAttribute('src','images/firefox-icon.png');
-    }
-}
