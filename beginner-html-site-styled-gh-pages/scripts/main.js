2c5ce98245c170c6f4cb2ee8e0f2997c873e9b87
Jüri Kajava <jyri.kajava@tptlive.ee>
2021-06-04 21:29:32 +0300

    Update main.js

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,13 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
