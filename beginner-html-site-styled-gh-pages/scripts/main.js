30829ac12edc33aa531932c565e6f693433de2b2
Jüri Kajava <jyri.kajava@tptlive.ee>
2021-06-04 17:22:35 +0300

    t

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,2 +1,14 @@
-const Heading = document.querySelector('h1');
-Heading.textContent = 'Hello world';
+let img = document.querySelector('image');
+
+img.onclick = function()
+    {
+        let src = img.getAttribute('src');
+        if(src === 'images/conflict.png')
+        {
+            img.setAttribute('src', 'images/firefox-icon.png');
+        }
+        else
+        {
+            img.setAttribute('src', 'images/conflict.png')
+        }
+    }
