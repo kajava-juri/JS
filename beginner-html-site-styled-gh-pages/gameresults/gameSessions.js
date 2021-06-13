def9e72b8ae59607d8b7932b97db677759cc0f63
Jüri Kajava <jyri.kajava@tptlive.ee>
2021-06-13 15:54:02 +0300

    added data saving for the game

---
+++ beginner-html-site-styled-gh-pages/gameresults/gameSessions.js
@@ -0,0 +1,7 @@
+// window.session0 = {"guesses" : "", "lastresult" : "", "loworhi" : ""}
+
+// function setResult(){
+    // session0['guesses'] = localStorage.getItem('guess');
+    // session0['lastresult'] = localStorage.getItem('lastresult');
+    // session0['loworhi'] = localStorage.getItem('loworhi');
+// }
--- beginner-html-site-styled-gh-pages/gameresults/scripts/gameMain.js
+++ beginner-html-site-styled-gh-pages/gameresults/scripts/gameMain.js
@@ -1,13 +1,7 @@
-const txt = document.querySelector('.place');
-//alert(mydata.name);
-//jsonName = mydata.name;
-//txt.textContent = jsonName;
-
 var countriesCount = countryList.length;
 let countryElement;
 const listOfCountries = document.querySelector('.lisOfCountries')
-let cntrBtn = document.querySelector('.countryButton');
-cntrBtn.onclick = function(){
+function cntrBtn(){
     for(let i = 0; i < countriesCount; i++){
         countryElement = document.createElement('li');
         countryElement.setAttribute('name', 'generatedcntr')
@@ -27,3 +21,50 @@ cdelBtn.onclick = function(){
   }
 }
 
+//========================================================
+
+const main = document.getElementById('main');
+
+var myVar = setInterval(checkIsAdded, 5000);
+
+function checkIsAdded(){
+  let hh = localStorage.getItem('isAdded');
+  console.log(localStorage.getItem('isAdded'));
+  if(hh == 1){
+    localStorage.setItem('isAdded', 0);
+    location.reload();
+  }
+  
+}
+
+setResults();
+
+function setResults(){
+  let results = localStorage.getItem('results');
+  let obj = JSON.parse(results);
+
+  
+  for(let i = 0; i < obj.result.length; i++){
+    line = document.createElement('hr');
+    resultDiv = document.createElement('div');
+    resultDiv.setAttribute('id', i + 1);
+    resultElementGU = document.createElement('p');
+    resultElementLA = document.createElement('p');
+    resultElementLO = document.createElement('p');
+    sessionNumber = document.createElement('h1');
+
+    sessionNumber.textContent = i + 1;
+    resultElementGU.textContent = obj.result[i].guess;
+    resultElementLA.textContent = obj.result[i].result;
+    resultElementLO.textContent = obj.result[i].number;
+
+    main.append(resultDiv);
+    resultDiv.append(sessionNumber);
+    resultDiv.append(resultElementGU);
+    resultDiv.append(resultElementLA);
+    resultDiv.append(resultElementLO);
+    resultDiv.append(line);
+  }
+
+  
+}
